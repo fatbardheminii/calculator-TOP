304bcbcf2eeba958d55a8474aecae1a6a9375c3a
fatbardheminii <fatbardh.emini@hotmail.com>
2024-01-03 22:26:21 +0100

    Add functions: mathematical, getEachNum, findIndex
    mathematical func: +-*/, getEachNum, find Index and Value of operator.

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 const allBtn = document.querySelectorAll('button');
 const displayableBtn = document.querySelectorAll('.btn');
-//const numberBtn = document.querySelectorAll('.number');
-//const operatorBtn = document.querySelectorAll('.operator');
+const numberBtn = document.querySelectorAll('.number');
+const operatorBtn = [...document.querySelectorAll('.operator')].map((el) => el.id);
 const userInput = document.querySelector('#user-input');
 const dotBtn = document.querySelector('.dot');
 const equalsBtn = document.querySelector('.btnEqual');
 const clearBtn = document.querySelector('.clear');
 const backspaceBtn = document.querySelector('.backspace');
-let expression = userInput.value;
+const numbersDisplayed = [];
+const operatorIndexes = [];
+const operatorsDisplayed = [];
 
 displayableBtn.forEach((btn) => btn.addEventListener("click", () => displayValue(btn)));
-
 function displayValue(clickedBtn){
     userInput.value += clickedBtn.id;
-    console.log(expression);
-    return expression;
+}
+
+function getEachNumSplitted(toCalculate) {
+    const splittedExpression = toCalculate.split(/[+\-*/]/);
+    splittedExpression.forEach(num => numbersDisplayed.push(num));
+}
+
+function add(firstNum, nextNum){
+    return +firstNum + +nextNum; 
+}
+
+function subtract(firstNum, nextNum) {
+  return +firstNum - +nextNum;
+}
+
+function multiply(firstNum, nextNum) {
+  return +firstNum * +nextNum;
+}
+
+function divide(firstNum, nextNum) {
+  return +firstNum / +nextNum;
+}
+
+function findOperatorValuesAndIndexes(expression) {
+    expression.split("").forEach((digit, index) => {
+        if(operatorBtn.includes(digit)){
+            operatorIndexes.push(index);
+            operatorsDisplayed.push(digit);
+        }
+    });
 }
 
 clearBtn.addEventListener("click", () => clear());
-
 function clear() {
     userInput.value = 0;
 }
 
 backspaceBtn.addEventListener("click", () => deleteDigit())
-
 function deleteDigit(){
     userInput.value = userInput.value.substring(0, userInput.value.length - 1);
+    findOperatorValuesAndIndexes(userInput.value);
+    getEachNumSplitted(userInput.value);
 }
